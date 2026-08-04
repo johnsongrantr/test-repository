5e725f53e2d3d4dbdfe1fd7b9e8bdc4bca5a7a33
johnsongrantr <grant.r.johnson6.civ@army.mil>
2026-08-04 12:52:20 -0700

    broken file

---
+++ broken_script.bat
@@ -0,0 +1,12 @@
+@echo off
+REM This script contains an obvious error for testing
+
+echo Starting test
+if "%1"=="test" goto test
+
+echo Argument was not test
+pause
+
+test:
+    echo Test branch reached
+    pause
